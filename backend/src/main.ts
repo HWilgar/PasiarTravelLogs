682bd5194de3a8ccc83c0966f1b8291f3bf1c911
wilgar <hwgcarino@gmail.com>
2024-07-12 23:03:21 +0800

    fix: filter exception bug15

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,18 +3,15 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
+import { CorsMiddleware } from './middleware/CorsMiddleware';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.use(new CorsMiddleware().use);
+
   app.use(helmet());
-
-  app.enableCors({
-    origin: 'https://pasiar-travel-logs.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  });
-
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
 }
