da8dfa680ad8284cd679f71db0382ead366186dc
wilgar <hwgcarino@gmail.com>
2024-07-12 21:55:08 +0800

    fix: filter exception bug6

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -15,7 +15,7 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Response } from 'express';
-import { HttpExceptionFilter } from './exception-filter/http-exception.filter';
+import { HttpExceptionFilter } from './exception-filter/http-exception.filter.ts';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { Public } from './auth/decorator/public.decorator';
