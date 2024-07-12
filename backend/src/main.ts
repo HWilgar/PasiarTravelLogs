d63c6b352588a9508bb05910e0ed5eb86f8c6ee0
wilgar <hwgcarino@gmail.com>
2024-07-12 22:48:40 +0800

    fix: filter exception bug12

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,11 +7,10 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-  app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
+  app.enableCors({
+    origin: 'https://pasiar-travel-logs.vercel.app',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Authorization',
   });
   app.setGlobalPrefix('api/v1');
   app.enableCors({
