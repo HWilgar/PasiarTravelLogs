df5276ef2ba6ef76456c7cdbd585088810f7ac3f
wilgar <hwgcarino@gmail.com>
2024-07-13 00:17:27 +0800

    fix: filter exception bug25

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,25 +4,21 @@ import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { CorsMiddleware } from './middleware/CorsMiddleware';
-import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use((req: Request, res: Response, next: NextFunction) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
-    next();
-  });
-
   app.enableCors({
-    allowedHeaders: '*',
-    origin: '*',
+    origin: ['http://localhost:3000', 'https://pasiar-travel-logs.vercel.app/'],
+    allowedHeaders: ['Accept', 'Content-Type'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
   });
 
-  app.use(helmet());
+  // app.use(helmet());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
 }
