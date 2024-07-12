c35b653281fbaee6a6d3843614be98be563b3eba
wilgar <hwgcarino@gmail.com>
2024-07-12 23:33:48 +0800

    fix: filter exception bug18

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,12 +4,23 @@ import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { CorsMiddleware } from './middleware/CorsMiddleware';
+import { Request, Response, NextFunction } from 'express';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.use(new CorsMiddleware().use);
+  app.use((req: Request, res: Response, next: NextFunction) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
+
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
 
   app.use(helmet());
   app.setGlobalPrefix('api/v1');
