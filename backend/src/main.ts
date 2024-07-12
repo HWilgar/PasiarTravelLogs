c23890652d0abd0012dc560278148706c2ec1a6f
wilgar <hwgcarino@gmail.com>
2024-07-12 21:09:47 +0800

    update: cors and vercel file

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,12 +2,22 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import helmet from 'helmet';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+    next();
+  });
   app.setGlobalPrefix('api/v1');
-  app.enableCors({});
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+  });
   app.use(helmet());
   await app.listen(process.env.PORT || 3000);
 }
