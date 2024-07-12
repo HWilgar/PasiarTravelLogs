0bc1e72d4b46643e1ff55ec9275423368c82e89e
wilgar <hwgcarino@gmail.com>
2024-07-12 22:55:40 +0800

    fix: filter exception bug14

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,19 +3,18 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { CorsMiddleware } from '@nest-middlewares/cors';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(helmet());
-  app.use(
-    CorsMiddleware.configure({
-      origin: 'https://pasiar-travel-logs.vercel.app',
-      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-      allowedHeaders: ['Content-Type', 'Authorization'],
-    }),
-  );
+
+  app.enableCors({
+    origin: 'https://pasiar-travel-logs.vercel.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
+
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
 }
