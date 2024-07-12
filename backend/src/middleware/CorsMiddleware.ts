4f163c620816ba83df7992053cf616c98d79458b
wilgar <hwgcarino@gmail.com>
2024-07-12 23:19:03 +0800

    fix: filter exception bug16

--- backend/src/middleware/CorsMiddleware.ts
+++ backend/src/middleware/CorsMiddleware.ts
@@ -4,13 +4,12 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
-    res.header('Access-Control-Allow-Origin', 'https://pasiar-travel-logs.vercel.app');
+    res.header(
+      'Access-Control-Allow-Origin',
+      'https://pasiar-travel-logs.vercel.app',
+    );
     res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    if (req.method === 'OPTIONS') {
-      res.sendStatus(200);
-    } else {
-      next();
-    }
+    next();
   }
 }
