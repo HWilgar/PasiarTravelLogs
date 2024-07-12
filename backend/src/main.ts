60e301d909ade6708e147b28fd66828300f439ad
wilgar <hwgcarino@gmail.com>
2024-07-13 07:45:09 +0800

    fix: cors issue2

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
 
   app.enableCors({
     origin: ['https://pasiar-travel-logs.vercel.app'],
-    allowedHeaders: ['Accept', 'Content-Type'],
+    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
     optionsSuccessStatus: 204,
