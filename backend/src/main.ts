ec4aa42c32744c0a588426c0a7af64891d4ffc01
wilgar <hwgcarino@gmail.com>
2024-07-12 19:37:44 +0800

    update: cors implementation

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,14 @@ import helmet from 'helmet';
 
 async function bootstrap() {
   dotenv.config();
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin:
+        'https://pasiar-travel-logs-nkbeno9qg-hwilgars-projects.vercel.app',
+      methods: ['GET', 'POST', 'PUT', 'DELETE'],
+      allowedHeaders: ['Content-Type', 'Authorization'],
+    },
+  });
   app.setGlobalPrefix('api/v1');
   app.use(helmet());
   await app.listen(process.env.PORT || 3000);
