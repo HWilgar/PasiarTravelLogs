60961a1f93c3c8322d2ccebfd5bbfcada7b41067
wilgar <hwgcarino@gmail.com>
2024-07-13 08:38:55 +0800

    feat: Setup for render

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,21 +3,11 @@ import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 import helmet from 'helmet';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import { CorsMiddleware } from './middleware/CorsMiddleware';
 
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
-
-  app.enableCors({
-    origin: ['https://pasiar-travel-logs.vercel.app'],
-    allowedHeaders: ['Accept', 'Content-Type', 'Authorization'],
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  });
-
+  app.enableCors();
   // app.use(helmet());
   app.setGlobalPrefix('api/v1');
   await app.listen(process.env.PORT || 3000);
