cd7715f8c5edd23c5d3b5f88d4d8988f198c92c7
wilgar <hwgcarino@gmail.com>
2024-07-17 19:16:42 +0800

    update: maints

--- backend/dist/main.js
+++ backend/dist/main.js
@@ -3,18 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
-const helmet_1 = require("helmet");
 async function bootstrap() {
     dotenv.config();
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
-    const corsOptions = {
-        origin: 'https://pasiar-travel-planner.vercel.app',
-        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true,
-        allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
-    };
-    app.enableCors(corsOptions);
-    app.use((0, helmet_1.default)());
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.setGlobalPrefix('api/v1');
     await app.listen(process.env.PORT || 3000);
 }
