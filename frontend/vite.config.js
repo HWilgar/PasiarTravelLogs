c4de6d07a2316e70b8f2b534351883ec9793c15b
wilgar <hwgcarino@gmail.com>
2024-07-22 12:04:31 +0800

    update: viteconfig

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,11 @@
 import react from '@vitejs/plugin-react-swc';
-// import { resolve, dirname } from 'node:path';
-// import { fileURLToPath } from 'url';
-import { defineConfig } from 'vite';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite';
 
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
 // https://vitejs.dev/config/
 // export default defineConfig(({
 //   plugins: [react() ],
@@ -28,25 +24,25 @@ export default defineConfig({
 //   },
 // }));
 
-// export default defineConfig(({ mode }) => {
-//   const env = loadEnv(mode, process.cwd(), '');
-//   return {
-//     define: {
-//       'process.env': env
-//     },
-//     plugins: [react()],
-//     resolve: {
-//       alias: {
-//         '@': resolve(__dirname, './src'),
-//       },
-//     },
-//     server: {
-//       proxy: {
-//         '/api/v1': {
-//           target: 'https://pasiar-travel-logs-api.vercel.app',
-//           changeOrigin: true
-//         },
-//       },
-//     },
-//   }
-// })
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    },
+    server: {
+      proxy: {
+        '/api/v1': {
+          target: 'https://pasiar-travel-logs-api.vercel.app',
+          changeOrigin: true
+        },
+      },
+    },
+  }
+})
