3c9aee0ef3ded1667e6770f437e24621afecb762
wilgar <hwgcarino@gmail.com>
2024-07-22 20:15:52 +0800

    update: vite and vercel file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc';
+import { resolve, dirname } from 'node:path';
+import { fileURLToPath } from 'url';
+import { defineConfig, loadEnv } from 'vite'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env': env
+    },
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+      },
+    }
+  }
 })
